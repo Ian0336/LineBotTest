aa6e507ff52f5646b94a7c55fdabc8a1aeb1ba23
YuYi <why33551@gmail.com>
2024-01-20 23:31:59 +0800

    add gunicorn

--- test.py
+++ test.py
@@ -63,5 +63,6 @@ def handle_message(event):
         TextSendMessage(text=event.message.text))
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
